93df84d4b6cdd97b0e273beaf7086042a9fc2486
MrMiner <syoa1996@gmail.com>
2018-09-18 22:50:31 +0900

    fix: before creating a new camera Component 모든 씬의 퍼즐 플레이는 적용이 되어 있는 상태로 커밋 함
    다음 커밋은 새로운 카메라 컴포넌트 예정

---
+++ Assets/scripts/Map/DestroyCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCheck : Switch
+{
+    [SerializeField]
+    private GameObject check_target;
+    
+    public new void Update()
+    {
+        this.OnOff = check_target == null ? true : false;
+    }
+}
