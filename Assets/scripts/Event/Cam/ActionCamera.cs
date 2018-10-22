c9c38822d2cdc015581cba85b90eabc0c506a676
MrMiner <syoa1996@gmail.com>
2018-10-23 02:41:03 +0900

    머지 끘

--- Assets/scripts/Event/Cam/ActionCamera.cs
+++ Assets/scripts/Event/Cam/ActionCamera.cs
@@ -66,30 +66,30 @@ public class ActionCamera : MonoBehaviour
     public bool GameStart;
     public float StartTime;
 
-    IEnumerator Zoom(float fov , bool zoom_in , float speed)
+    IEnumerator Zoom(float fov, bool zoom_in, float speed)
     {
         float cam_fov = cam.fieldOfView;
-        if( zoom_in )
+        if (zoom_in)
         {
-            while( cam.fieldOfView >= fov && cam.fieldOfView - 0.1f >= fov )
+            while (cam.fieldOfView >= fov && cam.fieldOfView - 0.1f >= fov)
             {
-                cam.fieldOfView = Mathf.Lerp(cam.fieldOfView , fov , Time.time * speed);
+                cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, fov, Time.time * speed);
                 yield return new WaitForFixedUpdate();
             }
         }
         else
         {
-            while( cam.fieldOfView <= fov && cam.fieldOfView - 0.1f <= fov )
+            while (cam.fieldOfView <= fov && cam.fieldOfView - 0.1f <= fov)
             {
-                cam.fieldOfView = Mathf.Lerp(cam.fieldOfView , fov , Time.time * speed);
+                cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, fov, Time.time * speed);
                 yield return new WaitForFixedUpdate();
             }
         }
     }
 
-    public void ZoomInOut(float fov , bool zoom_in , float speed)
+    public void ZoomInOut(float fov, bool zoom_in, float speed)
     {
-        StartCoroutine(Zoom(fov , zoom_in , speed));
+        StartCoroutine(Zoom(fov, zoom_in, speed));
     }
 
     public void SetState(State state)
@@ -98,9 +98,24 @@ public class ActionCamera : MonoBehaviour
         now_state = state;
     }
 
-    public void SetStateTarget(int num , State state , float _action_speed, float _guarantee_time, Vector3 angle, string _name, Vector3 _flow_dir)
+    public void SetStateTarget(Transform target, State state)
     {
         if(!has_camera_using)
+        {
+            SetTarget(target);
+            SetState(state);
+        }
+        else
+        {
+            EventInfo info = new EventInfo(state, target, 0, 0.3f, default_angle, "Player", Vector3.zero);
+            command.Enqueue(info);
+        }
+    }
+
+
+    public void SetStateTarget(int num, State state, float _action_speed, float _guarantee_time, Vector3 angle, string _name, Vector3 _flow_dir)
+    {
+        if (!has_camera_using)
         {
             has_camera_using = true;
             SetTarget(Pins[num]);
@@ -111,17 +126,17 @@ public class ActionCamera : MonoBehaviour
             Angle = angle;
             flow_dir = _flow_dir;
         }
-        else if(!now_event_name.Equals(_name))
+        else if (!now_event_name.Equals(_name))
         {
-            EventInfo info = new EventInfo(state, Pins[num], _guarantee_time, _action_speed, angle,_name, _flow_dir);
+            EventInfo info = new EventInfo(state, Pins[num], _guarantee_time, _action_speed, angle, _name, _flow_dir);
             command.Enqueue(info);
         }
 
     }
 
-    public void SetStateTarget(Transform transform , State state , float _action_speed, float _guarantee_time, Vector3 angle, string _name, Vector3 _flow_dir)
+    public void SetStateTarget(Transform transform, State state, float _action_speed, float _guarantee_time, Vector3 angle, string _name, Vector3 _flow_dir)
     {
-        if(!has_camera_using)
+        if (!has_camera_using)
         {
             has_camera_using = true;
             SetTarget(transform);
@@ -132,9 +147,9 @@ public class ActionCamera : MonoBehaviour
             Angle = angle;
             flow_dir = _flow_dir;
         }
-        else if(!now_event_name.Equals(_name))  //같은 이벤트는 연속으로 받지 않는다.
+        else if (!now_event_name.Equals(_name))  //같은 이벤트는 연속으로 받지 않는다.
         {
-            EventInfo info = new EventInfo(state, transform, _guarantee_time, _action_speed,angle,_name, _flow_dir);
+            EventInfo info = new EventInfo(state, transform, _guarantee_time, _action_speed, angle, _name, _flow_dir);
             command.Enqueue(info);
         }
     }
@@ -161,7 +176,7 @@ public class ActionCamera : MonoBehaviour
 
     public void SetTarget(Transform target)
     {
-        if( !target.Equals(now_target) )
+        if (!target.Equals(now_target))
         {
             cur_target = now_target;
             now_target = target;
@@ -170,10 +185,10 @@ public class ActionCamera : MonoBehaviour
 
     public void SetTarget(int number)
     {
-        if( !Pins[ number ].Equals(now_target) )
+        if (!Pins[number].Equals(now_target))
         {
             cur_target = now_target;
-            now_target = this.Pins[ number ];
+            now_target = this.Pins[number];
         }
     }
     int ignore;
@@ -184,12 +199,12 @@ public class ActionCamera : MonoBehaviour
         ignore = (1 << LayerMask.NameToLayer("Ground")) + (1 << LayerMask.NameToLayer("Ignore Raycast"));
         CalcPinDist cpd = FindObjectOfType<CalcPinDist>();
         ignore = ~ignore;
-        while( true )
+        while (true)
         {
             bool has_use_Zone = cpd == null ? false : cpd.enabled;
             if (!has_use_Zone)
             {
-                switch( now_state )
+                switch (now_state)
                 {
                     case State.Idle:
                         has_camera_using = false;
@@ -201,11 +216,11 @@ public class ActionCamera : MonoBehaviour
                         Vector3 pos = transform.position;
                         Vector3 tmp = now_target.position;
                         tmp.y += 1;
-                        Ray ray = new Ray(tmp , ( ( transform.position + Offset ) - tmp ).normalized);
-                        if( Physics.Raycast(ray , out hit , Vector3.Distance(tmp , transform.position) , ignore ))
+                        Ray ray = new Ray(tmp, ((transform.position + Offset) - tmp).normalized);
+                        if (Physics.Raycast(ray, out hit, Vector3.Distance(tmp, transform.position), ignore))
                         {
-                            if(!hit.collider.gameObject.CompareTag("Sword") 
-                                && hit.collider.gameObject.layer != LayerMask.NameToLayer("Object") 
+                            if (!hit.collider.gameObject.CompareTag("Sword")
+                                && hit.collider.gameObject.layer != LayerMask.NameToLayer("Object")
                                 && hit.collider.gameObject.layer != LayerMask.NameToLayer("Ignore Raycast")
                                 && hit.collider.gameObject.layer != LayerMask.NameToLayer("Ground")
                                 && hit.collider.gameObject.layer != LayerMask.NameToLayer("Totem")
@@ -220,54 +235,54 @@ public class ActionCamera : MonoBehaviour
                         }
                         else
                         {
-                            if(GameStart)
+                            if (GameStart)
                             {
-                                pos = Vector3.Lerp(pos , now_target.position + Offset , action_speed * 0.1f);
+                                pos = Vector3.Lerp(pos, now_target.position + Offset, action_speed * 0.1f);
                             }
                             else
                             {
-                                pos = Vector3.Lerp(pos , now_target.position + Offset , action_speed);
+                                pos = Vector3.Lerp(pos, now_target.position + Offset, action_speed);
                             }
                         }
-                        Debug.DrawRay(ray.origin , ray.direction * Vector3.Distance(tmp , transform.position + Offset) , Color.blue);
+                        Debug.DrawRay(ray.origin, ray.direction * Vector3.Distance(tmp, transform.position + Offset), Color.blue);
                         transform.position = pos;
                         has_camera_using = false;
                         //transform.eulerAngles = Angle;
-                        var ang = Quaternion.Lerp(transform.rotation , Quaternion.RotateTowards(transform.rotation , Quaternion.Euler(Angle) , action_speed * 10f), action_speed);
+                        var ang = Quaternion.Lerp(transform.rotation, Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Angle), action_speed * 10f), action_speed);
                         transform.rotation = ang;
                         break;
-                case State.Move_Pin:
-                    guarantee_timer += Time.deltaTime;
+                    case State.Move_Pin:
+                        guarantee_timer += Time.deltaTime;
 
-                    pos = Vector3.Lerp(transform.position , now_target.position , action_speed);
-                    transform.position = pos;
+                        pos = Vector3.Lerp(transform.position, now_target.position, action_speed);
+                        transform.position = pos;
 
-                    transform.rotation = Quaternion.RotateTowards(transform.rotation , Quaternion.Euler(Angle) , action_speed * 10f);
-                    if( Vector3.Distance(pos , now_target.position) >= 0.2f )
-                    {
-                        has_camera_using = true;
-                    }
-                    else
-                    {
+                        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Angle), action_speed * 10f);
+                        if (Vector3.Distance(pos, now_target.position) >= 0.2f)
+                        {
+                            has_camera_using = true;
+                        }
+                        else
+                        {
+                            end_state();
+                        }
+                        break;
+
+                    case State.Teleport_Pin:
+                        guarantee_timer += Time.deltaTime;
+                        transform.position = now_target.position;
+                        transform.rotation = Quaternion.Euler(Angle);
                         end_state();
-                    }
-                    break;
+                        break;
 
-                case State.Teleport_Pin:
-                    guarantee_timer += Time.deltaTime;
-                    transform.position = now_target.position;
-                    transform.rotation = Quaternion.Euler(Angle);
-                    end_state();
-                    break;
+                    case State.Flow:
+                        guarantee_timer += Time.deltaTime;
+                        transform.position += flow_dir * action_speed * Time.deltaTime;
+                        end_state();
+                        break;
 
-                case State.Flow:
-                    guarantee_timer += Time.deltaTime;
-                    transform.position += flow_dir * action_speed *Time.deltaTime;
-                    end_state();
-                    break;
-
+                }
             }
-            
             yield return new WaitForEndOfFrame();
         }
     }
@@ -285,9 +300,9 @@ public class ActionCamera : MonoBehaviour
 
     public void Update()
     {
-        if(GameStart)
+        if (GameStart)
         {
-            if(StartTime <= 0)
+            if (StartTime <= 0)
             {
                 StartTime = 0;
                 GameStart = false;
@@ -297,31 +312,31 @@ public class ActionCamera : MonoBehaviour
                 StartTime -= Time.deltaTime;
             }
         }
-        if( !has_camera_using )
+        if (!has_camera_using)
         {
-            if( command.Count != 0 )
+            if (command.Count != 0)
             {
                 SetStateTarget((EventInfo)command.Dequeue());
             }
         }
     }
 
-    IEnumerator ShakeCam(int tick , float power , float tick_by_tick_time)
+    IEnumerator ShakeCam(int tick, float power, float tick_by_tick_time)
     {
-        for( int i = 0 ; i < tick ; ++i )
+        for (int i = 0; i < tick; ++i)
         {
-            float v = Random.Range(-power , power);
-            float h = Random.Range(-power , power);
-            Vector3 shake = new Vector3(v , h, -v);
+            float v = Random.Range(-power, power);
+            float h = Random.Range(-power, power);
+            Vector3 shake = new Vector3(v, h, -v);
             transform.position += shake;
 
             yield return new WaitForSeconds(tick_by_tick_time);
         }
     }
 
-    public void Shake(int tick , float power , float tick_by_tick_time)
+    public void Shake(int tick, float power, float tick_by_tick_time)
     {
-        StartCoroutine(ShakeCam(tick , power , tick_by_tick_time));
+        StartCoroutine(ShakeCam(tick, power, tick_by_tick_time));
     }
 
     // Use this for initialization
