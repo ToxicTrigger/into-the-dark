474a2dd9aa7af18018e8c5ee3640fbb58267fbdc
MrMiner <syoa1996@gmail.com>
2018-10-26 00:19:09 +0900

    중간 보스 개선
    공격 모션 수정 + 패턴 변경

--- Assets/scripts/Aggro/AIAction.cs
+++ Assets/scripts/Aggro/AIAction.cs
@@ -48,14 +48,34 @@ public class AIAction : MonoBehaviour
         ani = GetComponent<Animator>();
     }
 
+    public float cool_down, attack_speed;
+    public float cooldown = 0;
+    public float attack_tick = 0;
+    public float cooltick = 0;
+    public int count = 0;
+
+    public bool attack_end;
+
+    /*  중간 보스 공격
+     *  해당 보스의 공격 패턴은 다음과 같다.
+     *  1. 플레이어 거리 확인
+     *  2. 사거리 내에 있을 때 공격신호 확인
+     *  3. 총 세번의 공격을 하는데 해당 공격이 끝나면 쿨타임을 가진 후 다시 3 반복
+     *  4. 중간에 만약 플레이어가 벗어난다면 공격 카운트 초기화 후 1 로 회귀
+     *  
+     *  가능하다면 보스의 쿨타임을 조절할 수 있도록 하는 것이 좋을 것 같음.
+     *  
+     *  공격을 이루는 요소는 공격과 공격 사이의 시간, 모든 공격이 끝나고 쿨타임이 있다.
+     */
     IEnumerator FSM()
     {
-        float attack_tick = 0;
+
         while( this.gameObject != null )
         {
             switch( state )
             {
                 case State.Action:
+                    FindObjectOfType<MidHp>().start = true;
                     ani.applyRootMotion = false;
                     ani.SetBool("isAction" , true);
                     yield return new WaitForSeconds(6);
@@ -66,23 +86,55 @@ public class AIAction : MonoBehaviour
 
                 case State.Attack:
                     transform.LookAt(Player.transform);
-                    if( attack_tick <= 2.0f )
+                    if(cooldown <= cool_down)
                     {
-                        attack_tick += Time.deltaTime;
-                        ani.SetBool("isAttack" , false);
+                        cooldown += Time.deltaTime;
                     }
                     else
                     {
-                        ani.SetBool("isAttack" , true);
-                        //yield return new WaitForSeconds(0.3f);
-                        Vector3 pos = transform.position;
-                        GameObject poison = Instantiate(Poison , pos , Quaternion.identity , null);
-                        poison.transform.position = transform.position;
-                        poison.GetComponent<Rigidbody>().useGravity = true;
-                        poison.GetComponent<Rigidbody>().velocity = ( Player.transform.position - pos ).normalized * Vector3.Distance(transform.position , Player.transform.position) / 2;
-                        poison.GetComponent<Rigidbody>().AddForce(Vector3.up * 10 , ForceMode.Impulse);
-                        Player.ac.Shake(4, 0.4f, Time.deltaTime);
-                        attack_tick = 0;
+                        if(count < 3)
+                        {
+                            if(cooltick <= 1)
+                            {
+                                if (attack_tick <= attack_speed)
+                                {
+                                    attack_tick += Time.deltaTime;
+                                    ani.SetBool("isAttack", false);
+                                }
+                                else
+                                {
+                                    ani.SetBool("isAttack", true);
+                                    //yield return new WaitForSeconds(0.3f);
+                                    Vector3 pos = transform.position;
+                                    GameObject poison = Instantiate(Poison, pos, Quaternion.identity, null);
+                                    poison.transform.position = transform.position;
+                                    poison.GetComponent<Rigidbody>().useGravity = true;
+                                    poison.GetComponent<Rigidbody>().velocity = (Player.transform.position - pos).normalized * Vector3.Distance(transform.position, Player.transform.position) / 2;
+                                    poison.GetComponent<Rigidbody>().AddForce(Vector3.up * 10, ForceMode.Impulse);
+                                    Player.ac.Shake(4, 0.4f, Time.deltaTime);
+                                    attack_tick = 0;
+                                    count += 1;
+                                    cooltick = 0;
+                                }
+
+                            }
+                            else
+                            {
+                                cooltick += Time.deltaTime;
+                            }
+                        }
+                        else
+                        {
+                            attack_end = true;
+                        }
+
+                    }
+                    if (attack_end)
+                    {
+                        ani.SetBool("isAttack", false);
+                        count = 0;
+                        cooldown = 0;
+                        attack_end = false;
                     }
                     break;
 
@@ -164,6 +216,11 @@ public class AIAction : MonoBehaviour
                 find_target = true;
             }
         }
+    }
+
+    private void OnDestroy()
+    {
+        FindObjectOfType<MidHp>().start = false;
     }
 
 }
---
+++ Assets/scripts/UI/MidHp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MidHp : MonoBehaviour
+{
+    public Color alpha;
+    public Image Red, Yellow, Back;
+    public bool start;
+    public Damageable damageable;
+
+    private void Start()
+    {
+        Color a = new Color(1,1,1,0);
+        Red.color = a;
+        Yellow.color = a;
+        Back.color = a;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (start)
+        {
+            alpha.a = Mathf.Lerp(alpha.a, 1, Time.deltaTime);
+        }
+        else
+        {
+            alpha.a = Mathf.Lerp(alpha.a, 0, Time.deltaTime);
+        }
+
+        Yellow.fillAmount = Mathf.Lerp(Yellow.fillAmount, damageable.Hp / damageable.Max_Hp, Time.deltaTime * 3);
+        Red.fillAmount = Mathf.Lerp(Red.fillAmount, Yellow.fillAmount, Time.deltaTime);
+
+        Red.color = alpha;
+        Yellow.color = alpha;
+        Back.color = alpha;
+	}
+}
