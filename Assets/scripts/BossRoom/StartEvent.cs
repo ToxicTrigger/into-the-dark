9638f6da28b0df98ee26a4b98e783ccaaea6760a
CK-400-191 <rak6474@naver.com>
2018-11-07 01:47:26 +0900

    이벤트

---
+++ Assets/scripts/BossRoom/StartEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartEvent : MonoBehaviour {
+    public GameObject _event;
+    bool play = false;
+    
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.O) && !play)
+        {
+            play = true;
+            _event.SetActive(true);
+        }
+	}
+}
