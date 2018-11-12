6c80a6569672111f02ccef65d08bf544b4ce4d83
MrMiner <syoa1996@gmail.com>
2018-11-12 21:42:18 +0900

    BGM 볼륨 조절
    거점 설정 수정

--- Assets/scripts/Map/Object/Switch.cs
+++ Assets/scripts/Map/Object/Switch.cs
@@ -9,22 +9,55 @@ public class Switch : Interactable
     public Particle_Handler[] ph;
     public AudioSource sound;
 
+    public float timer = 5;
+    private float now;
+
     private new void Awake()
     {
         base.Awake();
-
         ph = transform.GetComponents<Particle_Handler>();
-
         if( ph != null )
         {
             handlr = true;
         }
     }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Arrow"))
+        {
+            if (this.OnOff)
+            {
+                this.OnOff = false;
+            }
+            else
+            {
+                this.OnOff = true;
+            }
+        }
+    }
+
     public new void Update()
     {
         base.Update();
-        if( this.get_capture_area )
+        if (this.OnOff)
+        {
+            if (now <= timer)
+            {
+                now += Time.deltaTime;
+            }
+            else
+            {
+                now = 0;
+                this.OnOff = false;
+            }
+        }
+        else
+        {
+            now = 0;
+        }
+
+        if ( this.get_capture_area )
         {
             if( Input.GetButtonDown("Submit") )
             {
